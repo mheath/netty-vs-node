c3a81ea4b4692c5c6fd4fb744dfc1d9d3a3ba345
Gabriel Dayley <gabriel.dayley+git@gmail.com>
2011-09-30 11:32:08 -0600

    cleanup

--- js/server.js
+++ js/server.js
@@ -1,33 +1,18 @@
 var io = require('socket.io'),
-	express = require('express');
+    express = require('express');
 
 var app = express.createServer(),
-	io = io.listen(app);
+    io = io.listen(app);
 
 app.listen(3001);
 
 io.sockets.on('connection', function (socket) {
     var clientId = socket.id;
 
-	socket.emit('ack', { message: 'You are connected.' });
+    socket.emit('ack', { message: 'You are connected.' });
 
-	socket.on('next', function (data) {
-        console.log('next slide');
-		socket.broadcast.emit('next', data, clientId);
-  	});
-
-    socket.on('prev', function(data){
-        console.log('prev slide');
-        socket.broadcast.emit('prev', data, clientId);
+    socket.on('slideto', function(data) {
+        console.log('slideto', data);
+        socket.broadcast.emit('slideto', data);
     });
-
-
-    socket.on('draw', function(data){
-        console.log('draw');
-        socket.broadcast.emit('draw', data, clientId);
-    });
-
-    socket.on('clear-graffiti', function(){
-        socket.broadcast.emit('clear-graffiti');
-    })
 });
